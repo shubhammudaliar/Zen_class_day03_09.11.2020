6cfb6da6550efd70975ba38987c290f7ba49086e
shubhammudaliar <67221798+shubhammudaliar@users.noreply.github.com>
2020-11-27 09:44:37 +0530

    Add files via upload

---
+++ By_lat_lon.js
@@ -0,0 +1,19 @@
+
+// 1. create a request variable
+var request = new XMLHttpRequest();
+
+// open a connection
+request.open("GET", "http://api.openweathermap.org/data/2.5/weather?lat=44.89&lon=2.62&appid=2ca24a112adaa5e22f7c8246a75494f7" , true)
+
+// sending a request
+request.send();
+
+// load the response
+request.onload= function(){
+    var data= JSON.parse(this.response);
+    console.log(data);
+}
+
+// we are filtering flag key from all the object present in the url.
+//var flagurl = request.filter(newflag => newflag.flag);
+//console.log(flagurl);
